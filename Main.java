0e951ab1a3062f0d01914530f3f0f1f96b309ac8
simuuurggg <100079456+iremmdogann@users.noreply.github.com>
2023-06-27 11:55:16 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int fizik,kimya,mat,tarih,muzik,turkce;
-        Scanner giris= new Scanner(System.in);
+        double tutar,kdvOran =0.18 ,kdvLi,kdv;
+        System.out.print("tutarı griniz:");
+        Scanner giris = new Scanner(System.in);
+        tutar = giris.nextDouble();
 
-        System.out.println("Notlarınızı sırayla giriniz");
+        kdv=tutar*kdvOran;
+        kdvLi=kdv+tutar;
 
-             fizik= giris.nextInt();
-             kimya= giris.nextInt();
-             mat= giris.nextInt();
-             tarih= giris.nextInt();
-             muzik= giris.nextInt();
-             turkce= giris.nextInt();
-
-
-         int toplam =(fizik+kimya+mat+tarih+turkce+muzik);
-         double sonuc =toplam/6;
-
-        System.out.println(sonuc);
-
+        System.out.println("KDV'siz tutar:" +tutar );
+        System.out.println("KDV oran :" +kdvOran);
+        System.out.println("KDV:"+kdv);
+        System.out.println("KDV'li tutar: " + kdvLi);
 
     }
 }
