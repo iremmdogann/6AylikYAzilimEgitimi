919d47a014af5c3bb25a3ee37a03855ed4b32daa
simuuurggg <100079456+iremmdogann@users.noreply.github.com>
2023-06-30 21:30:44 +0300

    Add files via upload

---
+++ ucSayiSirala.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ucSayiSirala {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner input= new Scanner(System.in);
+        System.out.println("Sıralanacak sayıları giriniz");
+        a= input.nextInt();
+        b= input.nextInt();
+        c= input.nextInt();
+
+        if (a>b && a>c){
+             if (b>c){
+                 System.out.println("a>b>c");
+             } else System.out.println("a>c>b");
+        }
+        else if (b>a && b>c){
+            if (a>c){
+                System.out.println("b>a>c");
+            } else System.out.println("b>c>a");
+        }
+        else if (c>a && c>b) {
+            if (a>b){
+                System.out.println("c>a>b");
+            }else System.out.println("c>b>a");
+
+        }
+
+
+    }
+}
