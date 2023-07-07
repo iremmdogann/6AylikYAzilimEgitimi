3965105b4e4efe868d997a3a621f28e2521c2f23
simuuurggg <100079456+iremmdogann@users.noreply.github.com>
2023-07-07 23:05:52 +0300

    Add files via upload

---
+++ asalSayi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class asalSayi {
+    public static void main(String[] args) {
+        int a;
+
+      for (int i=2;i<=100;i++){
+          a=0;
+          for(int j=1;j<=i;j++){
+
+              if (i%j==0){
+                  a++;
+              }
+          }if (a==2){
+              System.out.print(i+"\t");
+          }
+      }
+
+    }
+}
