bf079f885cc13c48a4e9dab34f515410368befeb
simuuurggg <100079456+iremmdogann@users.noreply.github.com>
2023-08-20 23:55:24 +0300

    Add files via upload

---
+++ matrisTranspozu.java
@@ -0,0 +1,35 @@
+public class matrisTranspozu {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transpose = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        System.out.println("\nTransposed Matrix:");
+        printMatrix(transpose);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
